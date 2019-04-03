23cdc5010d0102c745ba24d738ff1a2bf17bed7c
Alistair Adcroft <Alistair.Adcroft@noaa.gov>
2019-04-03 18:26:11 -0400

    refineby2() works in 3d.
    - Also adds 3D rotate() to quickly move meshes around

--- python/GMesh.py
+++ python/GMesh.py
@@ -127,20 +127,75 @@ class GMesh:
         for j in range(0,self.nj+1,subsample):
             axis.plot(self.lon[j,:], self.lat[j,:], linecolor, **kwargs)
 
-    def refineby2(self):
+    def __lonlat_to_XYZ(lon, lat):
+        """Private method. Returns 3d coordinates (X,Y,Z) of spherical coordiantes (lon,lat)."""
+        deg2rad = np.pi/180.
+        lonr,latr = deg2rad*lon, deg2rad*lat
+        return np.cos( latr )*np.cos( lonr ), np.cos( latr )*np.sin( lonr ), np.sin( latr )
+
+    def __XYZ_to_lonlat(X, Y, Z):
+        """Private method. Returns spherical coordinates (lon,lat) of 3d coordinates (X,Y,Z)."""
+        rad2deg = 180./np.pi
+        lat = np.arcsin( Z ) * rad2deg # -90 .. 90
+        # Normalize X,Y to unit circle
+        sub_roundoff = 2./np.finfo(X[0,0]).max
+        R = 1. / ( np.sqrt(X*X + Y*Y) + sub_roundoff )
+        lon = np.arccos( R*X ) * rad2deg # 0 .. 180
+        lon = np.where( Y>=0, lon, -lon ) # Handle -180 .. 0
+        return lon,lat
+
+    def refineby2(self, work_in_3d=True):
         """Returns new Mesh instance with twice the resolution"""
-        lon = np.zeros( (2*self.nj+1, 2*self.ni+1) )
-        lat = np.zeros( (2*self.nj+1, 2*self.ni+1) )
-        #area = np.zeros( (2*self.nj, 2*self.ni) )
-        lon[::2,::2] = self.lon
-        lon[::2,1::2] = 0.5 * ( self.lon[:,:-1] + self.lon[:,1:] )
-        lon[1::2,::2] = 0.5 * ( self.lon[:-1,:] + self.lon[1:,:] )
-        lon[1::2,1::2] = 0.25 * ( ( self.lon[:-1,:-1] + self.lon[1:,1:] ) + ( self.lon[:-1,1:] + self.lon[1:,:-1] ) )
-        lat[::2,::2] = self.lat
-        lat[::2,1::2] = 0.5 * ( self.lat[:,:-1] + self.lat[:,1:] )
-        lat[1::2,::2] = 0.5 * ( self.lat[:-1,:] + self.lat[1:,:] )
-        lat[1::2,1::2] = 0.25 * ( ( self.lat[:-1,:-1] + self.lat[1:,1:] ) + ( self.lat[:-1,1:] + self.lat[1:,:-1] ) )
+
+        def local_refine(A):
+            """Retruns a refined variable a with shape (2*nj+1,2*ni+1) by linearly interpolation A with shape (nj+1,ni+1)."""
+            nj,ni = A.shape
+            a = np.zeros( (2*nj-1,2*ni-1) )
+            a[::2,::2] = A[:,:] # Shared nodes
+            a[::2,1::2] = 0.5 * ( A[:,:-1] + A[:,1:] ) # Mid-point along i-direction on original mesh
+            a[1::2,::2] = 0.5 * ( A[:-1,:] + A[1:,:] ) # Mid-point along j-direction on original mesh
+            a[1::2,1::2] = 0.25 * ( ( A[:-1,:-1] + A[1:,1:] ) + ( A[1:,:-1] + A[:-1,1:] ) ) # Mid-point of cell on original mesh
+            return a
+
+        if work_in_3d:
+            # Calculate 3d coordinates of nodes (X,Y,Z), Z points along pole, Y=0 at lon=0,180, X=0 at lon=+-90
+            X,Y,Z = GMesh.__lonlat_to_XYZ(self.lon, self.lat)
+
+            # Refine mesh in 3d and project onto sphere
+            X,Y,Z = local_refine(X), local_refine(Y), local_refine(Z)
+            R = 1. / np.sqrt((X*X + Y*Y) + Z*Z)
+            X,Y,Z = R*X, R*Y, R*Z
+
+            # Normalize X,Y to unit circle
+            #sub_roundoff = 2./np.finfo(X[0,0]).max
+            #R = 1. / ( np.sqrt(X*X + Y*Y) + sub_roundoff )
+            #X = R * X
+            #Y = R * Y
+
+            # Convert from 3d to spherical coordinates
+            lon,lat = GMesh.__XYZ_to_lonlat(X, Y, Z)
+
+        else:
+            lon,lat = local_refine(self.lon), local_refine(self.lat)
+
         return GMesh(lon=lon, lat=lat, rfl=self.rfl+1)
+
+    def rotate(self, y_rot=0, z_rot=0):
+        """Sequentially apply a rotation about the Y-axis and then the Z-axis."""
+        deg2rad = np.pi/180.
+        # Calculate 3d coordinates of nodes (X,Y,Z), Z points along pole, Y=0 at lon=0,180, X=0 at lon=+-90
+        X,Y,Z = GMesh.__lonlat_to_XYZ(self.lon, self.lat)
+        # Rotate anti-clockwise about Y-axis
+        C,S = np.cos( deg2rad*y_rot ), np.sin( deg2rad*y_rot )
+        X,Z = C*X + S*Z, -S*X + C*Z
+        # Rotate anti-clockwise about Y-axis
+        C,S = np.cos( deg2rad*z_rot ), np.sin( deg2rad*z_rot )
+        X,Y = C*X - S*Y, S*X + C*Y
+
+        # Convert from 3d to spherical coordinates
+        self.lon,self.lat = GMesh.__XYZ_to_lonlat(X, Y, Z)
+
+        return self
 
     def coarsenby2(self, coarser_mesh):
         """Set the height for lower level Mesh by coarsening"""
